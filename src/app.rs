a58c27140fa08e48a24bffffe28f46233ad97c7a
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-03-23 21:18:41 +0530

    feat: use correct sad icon

--- src/app.rs
+++ src/app.rs
@@ -16,7 +16,7 @@ pub fn App(cx: Scope) -> impl IntoView {
     view! {
         cx,
         <Stylesheet id="leptos" href="/pkg/sadgpt.css"/>
-        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
+        <Link rel="shortcut icon" type_="image/png" href="/sadgpt.png"/>
         <Title text="SadGPT" />
         <Router>
             <Routes>
