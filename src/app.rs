29a9fe15cdb38e5f81b52468e16bd1810d72e234
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-03-24 10:39:53 +0530

    refactor: self close `<path>` tag

--- src/app.rs
+++ src/app.rs
@@ -175,7 +175,7 @@ fn Home(cx: Scope) -> impl IntoView {
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
                                 d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                            ></path>
+                            />
                         </svg>
                     </button>
                 </form>
