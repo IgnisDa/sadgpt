09f6630139bc850d1bdb962c53624e8357cff789
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-03-26 07:18:58 +0530

    feat: use leptos-icons instead of svg

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,7 @@ use leptos::{
     ev::SubmitEvent,
     html::{Div, Input},
 };
+use leptos_icons::tb::Send::*;
 use leptos_meta::*;
 use leptos_router::*;
 use rand::{seq::SliceRandom, thread_rng, Rng};
@@ -164,7 +165,7 @@ fn Home(cx: Scope) -> impl IntoView {
             </div>
             <div class="py-6 w-full space-y-5 flex-none">
                 <form
-                    class="w-4/5 md:w-2/3 mx-auto flex items-center justify-center space-x-4"
+                    class="w-11/12 md:w-2/3 mx-auto flex items-center justify-center space-x-4"
                     on:submit=on_submit
                 >
                     <input
@@ -177,20 +178,7 @@ fn Home(cx: Scope) -> impl IntoView {
                     />
                     <button type="submit" aria-label="Submit">
                         <p class="sr-only">"Submit"</p>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke-width="1.5"
-                            stroke="currentColor"
-                            class="w-6 h-6 text-spt-white"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                            ></path>
-                        </svg>
+                        <Send color="white" size="25" />
                     </button>
                 </form>
                 <p class="text-spt-white text-center">
